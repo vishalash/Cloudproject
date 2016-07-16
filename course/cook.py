4ba66056aaa8564d6abdc88e735c9cf486527ba2
vishal <root@www.ash.com>
2016-07-16 18:09:18 +0530

    Final

--- course/cook.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-
-print "Set-Cookie:UserID=XYZ;\r\n"
-print "Set-Cookie:Password=XYZ123;\r\n"
-print "Set-Cookie:Expires=Tuesday, 31-Dec-2007 23:12:40 GMT";\r\n"
-print "Set-Cookie:Domain=www.tutorialspoint.com;\r\n"
-print "Set-Cookie:Path=/perl;\n"
-print "Content-type:text/html\r\n\r\n"
-print "hello <b>hi</b>"
